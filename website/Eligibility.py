64be2a732b27e82c0d41a5c93f32cdcdb2963de7
Swaphilo <micfaust@umich.edu>
2022-12-09 17:25:42 -0500

    Fixed single applicant in applicant datastore

--- website/Eligibility.py
+++ website/Eligibility.py
@@ -20,37 +20,70 @@ def eligibilityCheck():
 
     applicants = Applicant.query.order_by(Applicant.id) #Retrieve all applicants
     today = date.today()
-    for applicant in applicants:
-        applicantRegist = Registrar.query.filter_by(id=applicant.id).first() #Used to get dob from registrar
+    
+    if applicants is not iter: #Check if only one applicant in applicant datastore
+        applicantRegist = Registrar.query.filter_by(id=applicants[0].id).first()  # Used to get dob from registrar
         applicantDob = applicantRegist.dob
-        applicantDob = applicantDob.split('/') #splits the dob into a list where the / is present
-        applicantDob = list(map(int, applicantDob)) #turns the list from str to int
-        age = today.year - applicantDob[2] - ((today.month, today.day) < (applicantDob[0], applicantDob[1])) #gets the applicant's age based on today
-
-        if applicant.cGPA >= 3.2 and applicant.creditHrs >= 12 and age <= 23: #Eligibility rules fulfilled
-            applicant.eligibilityStatus = "Eligible"
+        applicantDob = applicantDob.split('/')  # splits the dob into a list where the / is present
+        applicantDob = list(map(int, applicantDob))  # turns the list from str to int
+        age = today.year - applicantDob[2] - ((today.month, today.day) < (
+        applicantDob[0], applicantDob[1]))  # gets the applicant's age based on today
+        
+        if applicants[0].cGPA >= 3.2 and applicants[0].creditHrs >= 12 and age <= 23: #Eligibility rules fulfilled
+            applicants[0].eligibilityStatus = "Eligible"
 
         else: #Eligibility rules not fulfilled
-            applicant.eligibilityStatus = "Non-Eligible"
+            applicants[0].eligibilityStatus = "Non-Eligible"
 
-            if applicant.cGPA < 3.2 and applicant.creditHrs < 12 and age > 23: #All eligibility rules failed
-                applicant.reason = "Does not meet any requirements."
+            if applicants[0].cGPA < 3.2 and applicants[0].creditHrs < 12 and age > 23: #All eligibility rules failed
+                applicants[0].reason = "Does not meet any requirements."
 
             else: #Checks for individual rules that failed
-                if applicant.cGPA < 3.2:
-                    applicant.reason = "Does not meet minimum GPA requirement."
+                if applicants[0].cGPA < 3.2:
+                    applicants[0].reason = "Does not meet minimum GPA requirement."
 
-                applicantReason = applicant.reason #Used to concatenate any additional rules broken to the reason attribute
+                applicantReason = applicants[0].reason #Used to concatenate any additional rules broken to the reason attribute
 
                 if applicantReason == None: # Used to check if passes cumulative GPA rule to clear None out of reason
                     applicantReason = ""
 
-                if applicant.creditHrs < 12:
-                    applicant.reason = f"{applicantReason} \n" + "Does not meet minimum credit hours requirement."
+                if applicants[0].creditHrs < 12:
+                    applicants[0].reason = f"{applicantReason} \n" + "Does not meet minimum credit hours requirement."
                 if age > 23:
-                    applicant.reason = f"{applicantReason} \n" "Does not meet maximum age requirement."
+                    applicants[0].reason = f"{applicantReason} \n" "Does not meet maximum age requirement."
 
-            db.session.commit()
+    else:
+        for applicant in applicants:
+            applicantRegist = Registrar.query.filter_by(id=applicant.id).first() #Used to get dob from registrar
+            applicantDob = applicantRegist.dob
+            applicantDob = applicantDob.split('/') #splits the dob into a list where the / is present
+            applicantDob = list(map(int, applicantDob)) #turns the list from str to int
+            age = today.year - applicantDob[2] - ((today.month, today.day) < (applicantDob[0], applicantDob[1])) #gets the applicant's age based on today
+
+            if applicant.cGPA >= 3.2 and applicant.creditHrs >= 12 and age <= 23: #Eligibility rules fulfilled
+                applicant.eligibilityStatus = "Eligible"
+
+            else: #Eligibility rules not fulfilled
+                applicant.eligibilityStatus = "Non-Eligible"
+
+                if applicant.cGPA < 3.2 and applicant.creditHrs < 12 and age > 23: #All eligibility rules failed
+                    applicant.reason = "Does not meet any requirements."
+
+                else: #Checks for individual rules that failed
+                    if applicant.cGPA < 3.2:
+                        applicant.reason = "Does not meet minimum GPA requirement."
+
+                    applicantReason = applicant.reason #Used to concatenate any additional rules broken to the reason attribute
+
+                    if applicantReason == None: # Used to check if passes cumulative GPA rule to clear None out of reason
+                        applicantReason = ""
+
+                    if applicant.creditHrs < 12:
+                        applicant.reason = f"{applicantReason} \n" + "Does not meet minimum credit hours requirement."
+                    if age > 23:
+                        applicant.reason = f"{applicantReason} \n" "Does not meet maximum age requirement."
+
+    db.session.commit()
 
     flash('Finished checking!', category='success')
     return redirect(url_for('cLogin.dashboard'))

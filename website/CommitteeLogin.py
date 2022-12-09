7e48f456c66054cb76b923c6f5727e18a9eb08b9
Swaphilo <micfaust@umich.edu>
2022-12-09 03:20:01 -0500

    Shows all applicants on committee dash

--- website/CommitteeLogin.py
+++ website/CommitteeLogin.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,render_template,request,flash, redirect, url_for
 from flask_login import logout_user, login_required, login_user
 from werkzeug.security import check_password_hash
+from .models import User, Applicant
+from flask_sqlalchemy import SQLAlchemy
 cLogin = Blueprint('cLogin',__name__) #blueprint for flask
-from .models import User
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -12,6 +13,11 @@ class loginForm(FlaskForm):
     userName = StringField("Username", validators=[DataRequired()])
     passWord= PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class applicantIDForm(FlaskForm):
+    id = StringField("Student ID", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @cLogin.route('/committee-login', methods = ['GET', 'POST'])
 def login():
@@ -23,7 +29,6 @@ def login():
         if user:
             if check_password_hash(user.passwordHash, form.passWord.data):
                 login_user(user)
-                flash('Hello!', category='success')
                 return redirect(url_for('cLogin.dashboard'))
             else:
                 flash('Wrong Password!', category='error')
@@ -36,4 +41,17 @@ def login():
 @cLogin.route('/committee-dash', methods = ['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template("Committee_dash.html")
+    form = applicantIDForm()
+
+    applicants = Applicant.query.order_by(Applicant.id)
+
+    print(applicants)
+
+    return render_template("Committee_dash.html", form=form, applicants=applicants)
+
+@cLogin.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('Logout Successful', category='success')
+    return redirect(url_for('views.home'))
